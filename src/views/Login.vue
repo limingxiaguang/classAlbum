668d4d4f2312f0713fa0b91af5645673573f4601
limingxiaguang <limingxiaguang@163.com>
2019-10-18 22:34:20 +0800

    修复登录页面样式

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -86,9 +86,14 @@ export default {
 </script>
 
 <style lang="less">
+html,body {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
 .ivu-form {
   width: 30%;
-  margin: 100px auto;
+  margin: 10% auto;
   & .ivu-input-wrapper {
     display: block;
     width: 100%;
@@ -104,8 +109,6 @@ export default {
   }
 }
 body {
-  width: 100%;
-  height: 100%;
   background-image: url("../assets/img/back_ground.jpg");
 
   background-repeat: no-repeat;
@@ -113,5 +116,6 @@ body {
   background-position: center center;
 
   background-size: cover;
+  
 }
 </style>
