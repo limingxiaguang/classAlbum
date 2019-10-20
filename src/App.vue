d3e7bd343718e849a41cb31c58934d62da329a5f
limingxiaguang <limingxiaguang@163.com>
2019-10-20 22:44:24 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@
 </template>
 
 <style lang="less">
+html,body, #app {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
