f27d42f53175e97997538a41aad8a123b3c37086
limingxiaguang <limingxiaguang@163.com>
2019-10-23 22:48:58 +0800

    初始 相册

---
+++ src/components/photoList.vue
@@ -0,0 +1,70 @@
+<template>
+<div>
+    <Row type="flex" justify="center">
+        <Col span="8">
+            <a @click.prevent="linkTo('1')">
+                <Card style="width:320px">
+                <div style="text-align:center">
+                    <img src="../assets/img/hh.jpg">
+                    <h3>A high quality UI Toolkit based on Vue.js</h3>
+                </div>
+            </Card>
+            </a>
+        </Col>
+        <Col span="8">
+            <a @click.prevent="linkTo('2')">
+                <Card style="width:320px">
+                <div style="text-align:center">
+                    <img src="../assets/img/hh.jpg">
+                    <h3>A high quality UI Toolkit based on Vue.js</h3>
+                </div>
+            </Card>
+            </a>
+        </Col>
+    </Row>
+    <Row type="flex" justify="center">
+        <Col span="8">
+            <a @click.prevent="linkTo('3')">
+                <Card style="width:320px">
+                <div style="text-align:center">
+                    <img src="../assets/img/hh.jpg">
+                    <h3>A high quality UI Toolkit based on Vue.js</h3>
+                </div>
+            </Card>
+            </a>
+        </Col>
+        <Col span="8">
+            <a @click.prevent="linkTo('4')">
+                <Card style="width:320px">
+                <div style="text-align:center">
+                    <img src="../assets/img/hh.jpg">
+                    <h3>A high quality UI Toolkit based on Vue.js</h3>
+                </div>
+            </Card>
+            </a>
+        </Col>
+    </Row>
+</div>
+</template>
+
+<script>
+export default {
+    name: "photoList",
+    methods: {
+        linkTo(index) {
+            console.log(index)
+        }
+    }
+}
+</script>
+
+<style lang="less">
+.ivu-card-body img {
+    display: block;
+    width: 100%;
+    height: 100%;
+}
+.ivu-row-flex-center {
+    margin: 2vw 0;
+}
+</style>
--- src/views/Album.vue
+++ src/views/Album.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    Album
+    <photo-list />
   </div>
 </template>
+<script>
+import photoList from "_c/photoList.vue";
+export default {
+  components: {
+    photoList
+  }
+}
+</script>
+<style lang="less" scoped>
+h2 {
+  text-align: center;
+  padding: 1vw;
+}
+</style>
